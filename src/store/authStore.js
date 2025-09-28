a20f1d7175626040f06e6afd85b5820daa1395b0
hadi <hadishamsi854@gmail.com>
2025-09-28 08:01:41 +0500

    feat: zustand firebase store

---
+++ src/store/authStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  user: null,
+  loading: true,
+  setUser: (user) => set({ user }),
+  setLoading: (loading) => set({ loading }),
+}));
+export default useAuthStore;
