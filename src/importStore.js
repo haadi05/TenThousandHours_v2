855020a980f5601d7375dc2a8d507159183d5bb6
hadi <hadishamsi854@gmail.com>
2025-09-28 11:07:54 +0500

    feat: central import/export store

---
+++ src/importStore.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./components/Navbar";
+export { default as Home } from "./components/Home";
+export { default as Dashboard } from "./components/Dashboard";
+export { default as LoginForm } from "./components/LoginForm";
