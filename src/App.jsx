d433ff41735b1f8a2ca078aba2df8715969c3ac5
hadi <hadishamsi854@gmail.com>
2025-09-29 06:32:17 +0500

    feat: loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./importStore";
+import useAuth from "./hooks/useAuth";
 
 function App() {
+  const { loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader"></div>
+      </div>
+    );
+  }
   return (
     <>
       <Navbar />
