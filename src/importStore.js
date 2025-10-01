e451b7a4f81b6b10b1d04ee30319b6a6c6d2969d
hadi <hadishamsi854@gmail.com>
2025-10-01 11:41:58 +0500

    chore: import/exports added

--- src/importStore.js
+++ src/importStore.js
@@ -1,5 +1,7 @@
 export { default as Navbar } from "./components/Navbar";
 export { default as Home } from "./components/Home";
+export { default as SkillsCard } from "./components/SkillsCard";
+export { default as AddCard } from "./components/AddCard";
 export { default as Dashboard } from "./components/Dashboard";
 export { default as LoginForm } from "./components/LoginForm";
 export { default as ProtectedRoutes } from "./components/ProtectedRoutes";
