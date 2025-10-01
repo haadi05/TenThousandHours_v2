1282c38fb094f2c940866572daa4da255f935807
hadi <hadishamsi854@gmail.com>
2025-10-01 11:49:01 +0500

    feat: grid layout added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
+import { SkillsCard, AddCard } from "../importStore";
+
 function Home() {
   return (
-    <div>
-      <div>home</div>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center px-18 py-4">
+        <div className="flex justify-between items-center w-full">
+          <p className="text-xl text-gray-400">Your Skills</p>
+        </div>
+        <div className="w-full grid grid-cols-4 gap-4 mt-4">
+          <SkillsCard />
+          <AddCard />
+        </div>
+      </div>
+    </>
   );
 }
 
