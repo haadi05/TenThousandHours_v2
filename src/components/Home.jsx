2951ac1e5e201ab316b0a7110d340848ddd246d8
hadi <hadishamsi854@gmail.com>
2025-10-02 23:21:07 +0500

    chore: minor style fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <>
       <div className="flex flex-col justify-center items-center px-18 py-4">
         <div className="flex justify-between items-center w-full">
-          <p className="text-xl text-gray-400">Your Skills</p>
+          <p className="text-xl text-secondary">Your Skills</p>
         </div>
         <div className="w-full grid grid-cols-4 gap-4 mt-4">
           <SkillsCard />
