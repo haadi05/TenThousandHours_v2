f8bedb70fb0e52468b143fd79aa60a4e18808cf8
hadi <hadishamsi854@gmail.com>
2025-10-07 20:49:14 +0500

    refactor: fix/update store

--- src/store/skillStore.js
+++ src/store/skillStore.js
@@ -2,7 +2,12 @@ import { create } from "zustand";
 
 const useSkillStore = create((set) => ({
   skills: [],
-  setSkill: (newSkill) =>
+  addSkill: (newSkill) =>
     set((state) => ({ skills: [...state.skills, newSkill] })),
+  updateSkill: (id, updates) =>
+    set((state) => ({
+      skills: state.skills.map((s) => (s.id === id ? { ...s, ...updates } : s)),
+    })),
 }));
+
 export default useSkillStore;
