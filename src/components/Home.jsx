2478d679f631d6ab81977d7e2af37fa9d5a5c66a
hadi <hadishamsi854@gmail.com>
2025-10-09 11:47:51 +0500

    feat: add skill logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import { SkillsCard, AddCard } from "../importStore";
+import useSkillStore from "../store/skillStore";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 
 function Home() {
+  const [open, setOpen] = useState(false);
+  const { skills } = useSkillStore();
+
   return (
-    <>
-      <div className="flex flex-col justify-center items-center px-18 py-4">
-        <div className="flex justify-between items-center w-full">
-          <p className="text-xl text-secondary">Your Skills</p>
-        </div>
-        <div className="w-full grid grid-cols-4 gap-4 mt-4">
-          <SkillsCard />
-          <AddCard />
-        </div>
+    <div className="flex flex-col justify-center items-center px-50 py-4">
+      <div className="flex justify-between items-center w-full">
+        <p className="text-xl text-secondary">Your Skills</p>
       </div>
-    </>
+
+      <div className="w-full grid grid-cols-3 gap-4 mt-4">
+        {skills.map((skill) => (
+          <div key={skill.id}>
+            <SkillsCard skillObj={skill} />
+          </div>
+        ))}
+        <AddCard />
+      </div>
+    </div>
   );
 }
 
