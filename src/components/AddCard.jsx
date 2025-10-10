0ef983c02d711fafae2e93cfd97387e4df910ac4
hadi <hadishamsi854@gmail.com>
2025-10-10 11:26:30 +0500

    fix: nested button hydration error

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -46,6 +46,8 @@ function AddCard() {
     };
 
     setSkill(newSkill);
+    setSkillName("");
+    setInitialHours(0);
     setOpenDialog(false);
   };
 
@@ -68,97 +70,99 @@ function AddCard() {
             </div>
           </Card>
         </DialogTrigger>
-        <DialogContent className="w-100">
+        <DialogContent aria-describedby={undefined} className="w-100">
           <DialogHeader>
             <DialogTitle className="text-2xl font-bold mb-4">
               Add New Skill
             </DialogTitle>
+          </DialogHeader>
 
-            <div>
-              <form onSubmit={handleSubmit}>
-                <div className="grid w-full items-center gap-2">
-                  <Label>Skill Name</Label>
-                  <Input
-                    type="text"
-                    value={skillName}
-                    onChange={(e) => setSkillName(e.target.value)}
-                    placeholder="e.g. Chess, Coding, Editing"
-                    className="mb-4"
-                  />
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className="grid w-full items-center gap-2">
+                <Label>Skill Name</Label>
+                <Input
+                  required
+                  type="text"
+                  value={skillName}
+                  onChange={(e) => setSkillName(e.target.value)}
+                  placeholder="e.g. Chess, Coding, Editing"
+                  className="mb-4"
+                />
 
-                  <div className="flex justify-center items-center gap-2">
-                    <div className="w-full flex flex-col gap-2">
-                      <Label>Initial hours</Label>
-                      <Input
-                        type="number"
-                        value={initialHours}
-                        onChange={(e) => setInitialHours(e.target.value)}
-                        min={0}
-                        className="no-arrows mb-4"
-                        placeholder="Hours Practiced Before (Optional)"
-                      />
-                    </div>
-
-                    <div className="w-full flex flex-col gap-2">
-                      <Label>Goal</Label>
-                      <Input
-                        type="number"
-                        value={goalHours}
-                        onChange={(e) => setGoalHours(e.target.value)}
-                        min={0}
-                        max={10000}
-                        className="no-arrows mb-4"
-                        placeholder="Enter your Goal Hours"
-                      />
-                    </div>
+                <div className="flex justify-center items-center gap-2">
+                  <div className="w-full flex flex-col gap-2">
+                    <Label>Initial hours</Label>
+                    <Input
+                      type="number"
+                      value={initialHours}
+                      onChange={(e) => setInitialHours(e.target.value)}
+                      min={0}
+                      className="no-arrows mb-4"
+                      placeholder="Hours Practiced Before (Optional)"
+                    />
                   </div>
 
-                  {/* Date btn */}
-                  <div className="flex flex-col gap-2">
-                    <Label htmlFor="date" className="px-1">
-                      Date
-                    </Label>
-                    <Popover open={open} onOpenChange={setOpen}>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant="outline"
-                          id="date"
-                          className="justify-between font-normal w-full mb-4"
-                        >
-                          {date ? date.toLocaleDateString() : "Select date"}
-                          <ChevronDownIcon />
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent
-                        className="w-auto overflow-hidden p-0"
-                        align="start"
+                  <div className="w-full flex flex-col gap-2">
+                    <Label>Goal</Label>
+                    <Input
+                      required
+                      type="number"
+                      value={goalHours}
+                      onChange={(e) => setGoalHours(e.target.value)}
+                      min={0}
+                      max={10000}
+                      className="no-arrows mb-4"
+                      placeholder="Enter your Goal Hours"
+                    />
+                  </div>
+                </div>
+
+                {/* Date btn */}
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="date" className="px-1">
+                    Date
+                  </Label>
+                  <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant="outline"
+                        id="date"
+                        className="justify-between font-normal w-full mb-4"
                       >
-                        <Calendar
-                          mode="single"
-                          selected={date}
-                          captionLayout="dropdown"
-                          onSelect={(date) => {
-                            setDate(date);
-                            setOpen(false);
-                          }}
-                        />
-                      </PopoverContent>
-                    </Popover>
-                  </div>
+                        {date ? date.toLocaleDateString() : "Select date"}
+                        <ChevronDownIcon />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="w-auto overflow-hidden p-0"
+                      align="start"
+                    >
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        captionLayout="dropdown"
+                        onSelect={(date) => {
+                          setDate(date);
+                          setOpen(false);
+                        }}
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
 
-                  {/* <Label>Theme</Label>
+                {/* <Label>Theme</Label>
                   <ThemeDropdown /> */}
 
-                  <Button
-                    className="cursor-pointer text-md font-bold hover:bg-secondary"
-                    type="submit"
-                  >
-                    Add Skill
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </DialogHeader>
+                <Button
+                  className="cursor-pointer text-md font-bold hover:bg-secondary"
+                  type="submit"
+                >
+                  Add Skill
+                </Button>
+              </div>
+            </form>
+          </div>
         </DialogContent>
       </Dialog>
     </>
