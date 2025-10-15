58c1ce1eae203b2cb7d02f68794435f935f31ac5
hadi <hadishamsi854@gmail.com>
2025-10-15 12:31:25 +0500

    style: loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./importStore";
 import useAuth from "./hooks/useAuth";
+import useSkillStore from "./store/skillStore";
+import { useEffect } from "react";
+import useAuthStore from "./store/authStore";
 
 function App() {
+  const { user } = useAuthStore();
   const { loading } = useAuth();
+  const { spinner, fetchSkills } = useSkillStore();
 
-  if (loading) {
+  useEffect(() => {
+    if (user?.uid) {
+      fetchSkills(user.uid);
+    }
+  }, [user, fetchSkills]);
+
+  if (loading || spinner) {
     return (
       <div className="flex justify-center items-center h-screen">
         <div className="loader"></div>
